091ad24629a1569fea0a237b9307abbf068059d0
Qian <qk2004@nyu.edu>
2025-05-26 16:16:56 +0800

    finish task 1

--- 1-producer-consumer/main.go
+++ 1-producer-consumer/main.go
@@ -10,22 +10,25 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
-func producer(stream Stream) (tweets []*Tweet) {
+func producer(stream Stream, tweets chan *Tweet) {
 	for {
 		tweet, err := stream.Next()
 		if err == ErrEOF {
-			return tweets
+			close(tweets)
+			return
 		}
 
-		tweets = append(tweets, tweet)
+		tweets <- tweet
 	}
 }
 
-func consumer(tweets []*Tweet) {
-	for _, t := range tweets {
+func consumer(wg *sync.WaitGroup, tweets chan *Tweet) {
+	defer wg.Done()
+	for t := range tweets {
 		if t.IsTalkingAboutGo() {
 			fmt.Println(t.Username, "\ttweets about golang")
 		} else {
@@ -38,11 +41,16 @@ func main() {
 	start := time.Now()
 	stream := GetMockStream()
 
-	// Producer
-	tweets := producer(stream)
+	tweets := make(chan *Tweet, 100)
 
+	// Producer
+	go producer(stream, tweets)
+
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
 	// Consumer
-	consumer(tweets)
+	go consumer(wg, tweets)
+	wg.Wait()
 
 	fmt.Printf("Process took %s\n", time.Since(start))
 }
